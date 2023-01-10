40a1011069aac4bc99b651c503ca75aeb88056d9
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-01-10 00:53:09 -0300

    se arreglo el url de back

--- src/app/servicios/service-panel-producto/panel-producto.service.ts
+++ src/app/servicios/service-panel-producto/panel-producto.service.ts
@@ -13,30 +13,30 @@ export class PanelProductoService {
   constructor(private http: HttpClient) { }
 
   getProductos(usuarioID: any) {
-    return this.http.get(this.url + '/administradors/' + usuarioID + '/productos');
+    return this.http.get(this.url + '/administradores/' + usuarioID + '/productos');
   }
 
   getProducto(productoID: any, usuarioID: any) {
-    return this.http.get(this.url + '/administradors/' + usuarioID + '/productos/' + productoID);
+    return this.http.get(this.url + '/administradores/' + usuarioID + '/productos/' + productoID);
   }
 
   crearProducto(producto: any, usuarioID: any) {
-    return this.http.post(this.url + '/administradors/' + usuarioID + '/productos', producto);
+    return this.http.post(this.url + '/administradores/' + usuarioID + '/productos', producto);
   }
 
   eliminarProducto(productoID: any, usuarioID: any) {
-    return this.http.delete(this.url + '/administradors/' + usuarioID + '/productos/' + productoID);
+    return this.http.delete(this.url + '/administradores/' + usuarioID + '/productos/' + productoID);
   }
 
   recuperarProducto(productoID: any, usuarioID: any) {
     let recuperar = {
       "activo": true
     }
-    return this.http.patch(this.url + '/administradors/' + usuarioID + '/productos/' + productoID, recuperar);
+    return this.http.patch(this.url + '/administradores/' + usuarioID + '/productos/' + productoID, recuperar);
   }
 
   modificarProducto(productoID: any, usuarioID: any, productoModificado: any) {
-    return this.http.patch(this.url + '/administradors/' + usuarioID + '/productos/' + productoID, productoModificado);
+    return this.http.patch(this.url + '/administradores/' + usuarioID + '/productos/' + productoID, productoModificado);
   }
 
   notificarProducto(producto: any) {
@@ -44,7 +44,7 @@ export class PanelProductoService {
   }
 
   subirImagen(productoID:any, usuarioID:any,formData: FormData) {
-    return this.http.post(this.url + '/administradors/' + usuarioID + '/productos/' + productoID + '/imagen', formData);
+    return this.http.post(this.url + '/administradores/' + usuarioID + '/productos/' + productoID + '/imagen', formData);
   }
 
 }
